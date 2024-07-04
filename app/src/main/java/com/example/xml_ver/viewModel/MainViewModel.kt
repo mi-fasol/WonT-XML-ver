95f9665cdba409adfe6a0325580d7ddb57b6a7d4
mi-fasol <minyoong39@naver.com>
2024-07-05 00:02:54 +0900

    feat: TextStyle 생성

---
+++ app/src/main/java/com/example/xml_ver/viewModel/MainViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.xml_ver.viewModel
+
+import android.content.Context
+import android.content.Intent
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.xml_ver.R
+import com.example.xml_ver.data.system.ColorOption
+import com.example.xml_ver.data.system.Event
+import com.example.xml_ver.util.SharedPreferenceUtil
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val context: Context
+) : ViewModel() {
+    var beforeStack = MutableStateFlow("mainScreen")
+
+    private val _mainColor = MutableStateFlow(ColorOption.BLUE)
+    val mainColor: StateFlow<ColorOption> = _mainColor
+
+    private val _colorState = MutableStateFlow(R.color.mainColor)
+    val colorState: StateFlow<Int> = _colorState
+
+    val role = SharedPreferenceUtil(context).getString("role", "USER")
+    val nickname = SharedPreferenceUtil(context).getString("nickname", "")
+
+    private val _notificationState =
+        MutableStateFlow(SharedPreferenceUtil(context).getBoolean("notification", false))
+    val notificationState: StateFlow<Boolean> = _notificationState
+
+    fun changeNotificationAccess(enabled: Boolean) {
+        viewModelScope.launch {
+            _notificationState.value = enabled
+            SharedPreferenceUtil(context).setBoolean("notification", enabled)
+        }
+    }
+
+    init {
+        val mainColor = SharedPreferenceUtil(context).getInt("themeColor", R.color.mainColor)
+        _colorState.value = mainColor
+    }
+
+    fun updateColor(colorOption: ColorOption) {
+        SharedPreferenceUtil(context).setInt("themeColor", colorOption.colorResId)
+        viewModelScope.launch {
+            _mainColor.emit(colorOption)
+            _colorState.emit(colorOption.colorResId)
+        }
+    }
+
+    fun getVersion(): String? {
+        var versionName = ""
+        val pm = context.packageManager.getPackageInfo(context.packageName, 0)
+        versionName = pm.versionName
+
+        return versionName
+    }
+
+    private val _navigateToAnotherActivity = MutableLiveData<Event<Intent>>()
+    val navigateToAnotherActivity: LiveData<Event<Intent>> = _navigateToAnotherActivity
+}
---
+++ app/src/main/java/com/example/xml_ver/viewModel/user/LoginViewModel.kt
@@ -0,0 +1,115 @@
+package com.example.xml_ver.viewModel.user
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.haemo_kotlin.network.Resource
+import com.example.xml_ver.data.retrofit.user.LoginModel
+import com.example.xml_ver.repository.UserRepository
+import com.example.xml_ver.util.SharedPreferenceUtil
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val repository: UserRepository,
+    private val context: Context
+) : ViewModel() {
+
+    enum class LoginUserState { SUCCESS, LOGIN, NONE, BEFORE }
+
+    private val _loginState = MutableStateFlow<Resource<LoginModel>>(Resource.loading(null))
+    val loginState: StateFlow<Resource<LoginModel>> = _loginState.asStateFlow()
+
+    private val _userState = MutableStateFlow(LoginUserState.NONE)
+    val loginUser: StateFlow<LoginUserState> = _userState.asStateFlow()
+
+    private val _isLoginSuccess = MutableStateFlow(false)
+    val isLoginSuccess: StateFlow<Boolean> = _isLoginSuccess.asStateFlow()
+
+    val id = MutableStateFlow("")
+    val pwd = MutableStateFlow("")
+
+    var isValid: StateFlow<Boolean> = combine(id, pwd) { id, pwd ->
+        id.isNotBlank() && pwd.isNotBlank()
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), false)
+
+    suspend fun checkUserExists() {
+        _userState.value = LoginUserState.BEFORE
+        val studentId = SharedPreferenceUtil(context).getString("studentId", "").toString()
+        Log.d("studentID: ", studentId)
+        if (studentId.isNotBlank()) {
+            viewModelScope.launch {
+                try {
+                    Log.d("미란 ", "실행은 됨")
+                    Log.d("미란 이전 state", _userState.value.toString())
+                    val response = repository.checkHaveAccount(studentId.toInt())
+                    if (response.isSuccessful && response.body() != null) {
+                        val responseUId = response.body()
+                        if (responseUId == 0) {
+                            _userState.value = LoginUserState.LOGIN
+                            Log.d("미란", "회원가입 필요")
+                        } else {
+                            _userState.value = LoginUserState.SUCCESS
+                            SharedPreferenceUtil(context).setInt("uId", responseUId!!)
+                            Log.d("미란 유저", responseUId.toString())
+                        }
+                        Log.d("미란 state", _userState.value.toString())
+                    } else {
+                        Log.e("API Exception", "널로 받아짐")
+                    }
+                } catch (e: Exception) {
+                    Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+                }
+            }
+        } else {
+            Log.d("미란 ", "실행은 됨 ${_userState.value.toString()}")
+            _userState.value = LoginUserState.NONE
+            Log.d("미란 ", "에효효 ${_userState.value.toString()}")
+        }
+    }
+
+    fun login(id: String, pwd : String) {
+        _isLoginSuccess.value = false
+        Log.d("미란", "일단 실행은 됐쓰요~")
+        viewModelScope.launch {
+            _loginState.value = Resource.loading(null)
+            try {
+                val loginModel = LoginModel(id, pwd)
+                val response = repository.tryLogin(loginModel)
+                if (response.isSuccessful && response.body() != null) {
+                    val isSuccess = response.body()
+                    if (isSuccess != null) {
+                        _isLoginSuccess.value = isSuccess
+                    }
+                    if (isSuccess!!) {
+                        SharedPreferenceUtil(context).setString("studentId", id)
+                    }
+                    Log.d("stId: ", SharedPreferenceUtil(context).getString("studentId", "").toString())
+                    Log.d("로그인 결과", _isLoginSuccess.value.toString())
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "로그인 에러 응답: $errorBody")
+                    _loginState.value = Resource.error(response.errorBody().toString(), null)
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "로그인 요청 중 예외 발생: ${e.message}")
+                _loginState.value = Resource.error(e.message ?: "An error occurred", null)
+            }
+        }
+    }
+
+    fun signOut(mainColor: Int) {
+        SharedPreferenceUtil(context).removeAll()
+        SharedPreferenceUtil(context).setInt("themeColor", mainColor)
+    }
+}
