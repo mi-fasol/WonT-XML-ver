2bf4f232082b286f2d6fa8a37769264ab883abe6
mi-fasol <minyoong39@naver.com>
2024-07-14 22:20:55 +0900

    refactor: MeetingFragment 리팩토링

--- app/src/main/java/com/example/xml_ver/ui/main/board/MeetingFragment.kt
+++ app/src/main/java/com/example/xml_ver/ui/main/board/MeetingFragment.kt
@@ -12,7 +12,6 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.xml_ver.R
-import com.example.xml_ver.data.retrofit.post.PostResponseModel
 import com.example.xml_ver.databinding.FragmentMeetingBinding
 import com.example.xml_ver.ui.adapter.PostAdapter
 import com.example.xml_ver.ui.adapter.TodayPostAdapter
@@ -20,7 +19,6 @@ import com.example.xml_ver.viewModel.MainViewModel
 import com.example.xml_ver.viewModel.board.AcceptationViewModel
 import com.example.xml_ver.viewModel.board.MeetingViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -50,45 +48,30 @@ class MeetingFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         navController = findNavController()
 
-        todayPostAdapter = TodayPostAdapter(navController)
+        setupRecyclerViews()
+        setupToolbar(view)
+        getPostData()
+    }
 
+    private fun setupRecyclerViews() {
+        setupTodayRecyclerView()
+        setupPostRecyclerView()
+    }
+
+    private fun setupTodayRecyclerView() {
+        todayPostAdapter = TodayPostAdapter(navController)
         binding.todayPostView.apply {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
             adapter = todayPostAdapter
         }
+    }
 
+    private fun setupPostRecyclerView() {
         postAdapter =
             PostAdapter(postViewModel, acceptationViewModel, mainViewModel, navController)
         binding.postListView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = postAdapter
-        }
-
-        setupToolbar(view)
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            postViewModel.getPost()
-            postViewModel.postModelList.collect {
-                postAdapter.submitList(it)
-            }
-        }
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            postViewModel.getTodayPost()
-            postViewModel.getPost()
-            postViewModel.postModelList.collect { posts ->
-                todayPostAdapter.updatePosts(posts)
-                val params = binding.postListView.layoutParams as ConstraintLayout.LayoutParams
-                if (posts.isEmpty()) {
-                    binding.todayPostView.visibility = View.GONE
-                    params.height = ViewGroup.LayoutParams.MATCH_PARENT
-                } else {
-                    binding.todayPostView.visibility = View.VISIBLE
-                    params.height = (ViewGroup.LayoutParams.MATCH_PARENT * 0.62).toInt()
-                }
-                binding.postListView.layoutParams = params
-                binding.postListView.requestLayout()
-            }
         }
     }
 
@@ -97,5 +80,44 @@ class MeetingFragment : Fragment() {
         toolbar.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
+    }
+
+    private fun getPostData() {
+        lifecycleScope.launch {
+            getTodayPostList()
+            getPostList()
+        }
+    }
+
+    private fun getPostList() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            postViewModel.getPost()
+            postViewModel.postModelList.collect {
+                postAdapter.submitList(it)
+            }
+        }
+    }
+
+    private fun getTodayPostList() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            postViewModel.getTodayPost()
+            postViewModel.todayPostList.collect { posts ->
+                todayPostAdapter.updatePosts(posts)
+                updateTodayPostViewVisibility(posts.isEmpty())
+            }
+        }
+    }
+
+    private fun updateTodayPostViewVisibility(isEmpty: Boolean) {
+        val params = binding.postListView.layoutParams as ConstraintLayout.LayoutParams
+        binding.todayPostView.visibility = if (isEmpty) {
+            params.height = ViewGroup.LayoutParams.MATCH_PARENT
+            View.GONE
+        } else {
+            params.height = (ViewGroup.LayoutParams.MATCH_PARENT * 0.62).toInt()
+            View.VISIBLE
+        }
+        binding.postListView.layoutParams = params
+        binding.postListView.requestLayout()
     }
 }
