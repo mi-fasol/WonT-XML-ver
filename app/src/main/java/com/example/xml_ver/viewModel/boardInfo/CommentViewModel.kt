e3c2fbe419180a657a2ca98da401be22951af4d9
mi-fasol <minyoong39@naver.com>
2024-07-21 18:35:27 +0900

    feat: Wish, Comment ViewModel 생성

---
+++ app/src/main/java/com/example/xml_ver/viewModel/boardInfo/CommentViewModel.kt
@@ -0,0 +1,289 @@
+package com.example.haemo_kotlin.viewModel.boardInfo
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.haemo_kotlin.network.Resource
+import com.example.xml_ver.data.retrofit.comment.comment.CommentModel
+import com.example.xml_ver.data.retrofit.comment.comment.CommentResponseModel
+import com.example.xml_ver.data.retrofit.comment.reply.ReplyModel
+import com.example.xml_ver.data.retrofit.comment.reply.ReplyResponseModel
+import com.example.xml_ver.data.retrofit.user.UserResponseModel
+import com.example.xml_ver.repository.CommentRepository
+import com.example.xml_ver.util.SharedPreferenceUtil
+import com.example.xml_ver.util.getCurrentDateTime
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class CommentViewModel @Inject constructor(
+    private val repository: CommentRepository,
+    private val context: Context
+) : ViewModel() {
+
+    val content = MutableStateFlow("")
+
+    private val _commentList = MutableStateFlow<List<CommentResponseModel>>(emptyList())
+    val commentList: StateFlow<List<CommentResponseModel>> = _commentList
+
+//    private val _replyList = MutableStateFlow<List<ReplyResponseModel>>(emptyList())
+//    val replyList: StateFlow<List<ReplyResponseModel>> = _replyList
+
+    private val _replyList = MutableStateFlow<MutableMap<Int, List<ReplyResponseModel>>>(HashMap())
+    val replyList: StateFlow<MutableMap<Int, List<ReplyResponseModel>>> = _replyList
+
+
+    private val _userList = MutableStateFlow<List<UserResponseModel>>(emptyList())
+    val userList: StateFlow<List<UserResponseModel>> = _userList
+
+    private val _replyUserList =
+        MutableStateFlow<MutableMap<Int, List<UserResponseModel>>>(HashMap())
+    val replyUserList: StateFlow<MutableMap<Int, List<UserResponseModel>>> = _replyUserList
+
+    private val _commentRegisterState =
+        MutableStateFlow<Resource<CommentResponseModel>>(Resource.loading(null))
+    val commentRegisterState: StateFlow<Resource<CommentResponseModel>> =
+        _commentRegisterState.asStateFlow()
+
+    private val _replyRegisterState =
+        MutableStateFlow<Resource<ReplyResponseModel>>(Resource.loading(null))
+    val replyRegisterState: StateFlow<Resource<ReplyResponseModel>> =
+        _replyRegisterState.asStateFlow()
+
+    var isReply = MutableStateFlow(false)
+    var commentId = MutableStateFlow(0)
+    var beforeCId = MutableStateFlow(0)
+    var postType = MutableStateFlow(0)
+
+    suspend fun getCommentListByPId(pId: Int, type: Int) {
+        viewModelScope.launch {
+            try {
+                val response = when (type) {
+                    1 -> repository.getCommentListByPId(pId)
+                    2 -> repository.getClubCommentListByPId(pId)
+                    3 -> repository.getHotPlaceCommentListByPId(pId)
+                    else -> throw (error("오류낫심"))
+                }
+                if (response.isSuccessful && response.body() != null) {
+                    val commentList = response.body()
+                    _commentList.value = commentList!!
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "댓글 유저 에러 응답: $errorBody")
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+            }
+        }
+    }
+
+    suspend fun getCommentUser(pId: Int, type: Int) {
+        viewModelScope.launch {
+            try {
+                val response = when (type) {
+                    1 -> repository.getCommentUser(pId)
+                    2 -> repository.getClubCommentUser(pId)
+                    3 -> repository.getHotPlaceCommentUser(pId)
+                    else -> throw (error("오류낫심"))
+                }
+                if (response.isSuccessful && response.body() != null) {
+                    val response = response.body()
+                    _userList.value = response!!
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 하나 에러 응답: $errorBody")
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+            }
+        }
+    }
+
+    fun registerComment(text: String, pId: Int, type: Int) {
+        val nickname = SharedPreferenceUtil(context).getString("nickname", "").toString()
+        val comment = when (type) {
+            1 -> CommentModel(
+                pId,
+                null,
+                null,
+                text,
+                nickname,
+                getCurrentDateTime()
+            )
+
+            2 -> CommentModel(
+                null,
+                pId,
+                null,
+                text,
+                nickname,
+                getCurrentDateTime()
+            )
+
+            3 -> CommentModel(
+                null,
+                null,
+                pId,
+                text,
+                nickname,
+                getCurrentDateTime()
+            )
+
+            else -> throw (error("에러러링"))
+        }
+
+        Log.d("미란 comment", comment.toString())
+
+        viewModelScope.launch {
+            _commentRegisterState.value = Resource.loading(null)
+            try {
+                val response = when (type) {
+                    1 -> repository.saveComment(comment)
+                    2 -> repository.saveClubComment(comment)
+                    3 -> repository.saveHotPlaceComment(comment)
+                    else -> throw (error("오류낫심"))
+                }
+                if (response.isSuccessful && response.body() != null) {
+                    val savedComment = response.body()
+                    _commentRegisterState.value = Resource.success(savedComment)
+                    _commentList.value += savedComment!!
+                    Log.d("미란 SaveComment", savedComment.toString())
+                    commentId.value = 0
+                    isReply.value = false
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "에러 응답: $errorBody")
+                    _commentRegisterState.value =
+                        Resource.error(response.errorBody().toString(), null)
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "로그인 요청 중 예외 발생: ${e.message}")
+                _commentRegisterState.value = Resource.error(e.message ?: "An error occurred", null)
+            }
+        }
+    }
+
+    // 대댓글
+    suspend fun getReplyListByCId(cId: Int, type: Int) {
+        viewModelScope.launch {
+            try {
+                val response = when (type) {
+                    1 -> repository.getReplyListByCId(cId)
+                    2 -> repository.getClubReplyListByCId(cId)
+                    3 -> repository.getHotPlaceReplyListByCId(cId)
+                    else -> throw (error("오류낫심"))
+                }
+                if (response.isSuccessful && response.body() != null) {
+                    val replyList = response.body()
+                    replyList?.let { list ->
+                        _replyList.value[cId] = list
+                    }
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 하나 에러 응답: $errorBody")
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+            }
+        }
+    }
+
+    suspend fun getReplyUser(cId: Int, type: Int) {
+        viewModelScope.launch {
+            try {
+                val response = when (type) {
+                    1 -> repository.getReplyUserByCId(cId)
+                    2 -> repository.getClubReplyUserByCId(cId)
+                    3 -> repository.getHotPlaceReplyUserByCId(cId)
+                    else -> throw (error("오류낫심"))
+                }
+                if (response.isSuccessful && response.body() != null) {
+                    val response = response.body()
+                    response?.let { list ->
+                        _replyUserList.value[cId] = list
+                    }
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 하나 에러 응답: $errorBody")
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+            }
+        }
+    }
+
+    fun registerReply(text: String, cId: Int, type: Int) {
+        val nickname = SharedPreferenceUtil(context).getString("nickname", "").toString()
+        val reply = when (type) {
+            1 -> ReplyModel(
+                cId,
+                null,
+                null,
+                text,
+                nickname,
+                getCurrentDateTime()
+            )
+
+            2 -> ReplyModel(
+                null,
+                cId,
+                null,
+                text,
+                nickname,
+                getCurrentDateTime()
+            )
+
+            3 -> ReplyModel(
+                null,
+                null,
+                cId,
+                text,
+                nickname,
+                getCurrentDateTime()
+            )
+
+            else -> throw (error("에러러링"))
+        }
+
+        viewModelScope.launch {
+            _commentRegisterState.value = Resource.loading(null)
+            try {
+                val response = when (type) {
+                    1 -> repository.saveReply(reply)
+                    2 -> repository.saveClubReply(reply)
+                    3 -> repository.saveHotPlaceReply(reply)
+                    else -> throw (error("오류낫심"))
+                }
+                if (response.isSuccessful && response.body() != null) {
+                    val savedReply = response.body()
+                    _replyRegisterState.value = Resource.success(savedReply)
+                    val beforeList = _replyList.value[cId]
+                    if (beforeList.isNullOrEmpty()) {
+                        Log.d("미란 nullOrEmpty", "들어와졋내요..")
+                        _replyList.value[cId] = emptyList()
+                    }
+                    _replyList.value[cId] = _replyList.value[cId]!! + savedReply!!
+                    Log.d("미란 SavedReply", savedReply.toString())
+                    commentId.value = 0
+                    beforeCId.value = cId
+                    isReply.value = false
+                    _replyRegisterState.value = Resource.loading(null)
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "에러 응답: $errorBody")
+                    _replyRegisterState.value =
+                        Resource.error(response.errorBody().toString(), null)
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "로그인 요청 중 예외 발생: ${e.message}")
+                _replyRegisterState.value = Resource.error(e.message ?: "An error occurred", null)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/xml_ver/viewModel/boardInfo/WishViewModel.kt
@@ -0,0 +1,271 @@
+package com.example.haemo_kotlin.viewModel.boardInfo
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.haemo_kotlin.network.Resource
+import com.example.xml_ver.data.retrofit.post.ClubPostResponseModel
+import com.example.xml_ver.data.retrofit.post.HotPlaceResponsePostModel
+import com.example.xml_ver.data.retrofit.post.PostResponseModel
+import com.example.xml_ver.data.retrofit.wish.WishListModel
+import com.example.xml_ver.repository.WishListRepository
+import com.example.xml_ver.util.SharedPreferenceUtil
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class WishViewModel @Inject constructor(
+    private val repository: WishListRepository,
+    private val context: Context
+) : ViewModel() {
+
+    private val uId = SharedPreferenceUtil(context).getInt("uId", 0)
+    var pId = MutableStateFlow(0)
+
+    private val _isDeleted = MutableStateFlow<Boolean>(false)
+    val isDeleted: StateFlow<Boolean> = _isDeleted
+
+    private val _wishMeetingList = MutableStateFlow<List<PostResponseModel>>(emptyList())
+    val wishMeetingList: StateFlow<List<PostResponseModel>> = _wishMeetingList
+
+    private val _wishClubList = MutableStateFlow<List<ClubPostResponseModel>>(emptyList())
+    val wishClubList: StateFlow<List<ClubPostResponseModel>> = _wishClubList
+
+    private val _wishHotPlaceList = MutableStateFlow<List<HotPlaceResponsePostModel>>(emptyList())
+    val wishHotPlaceList: StateFlow<List<HotPlaceResponsePostModel>> = _wishHotPlaceList
+
+    private val _postModelState =
+        MutableStateFlow<Resource<PostResponseModel>>(Resource.loading(null))
+    val postModelState: StateFlow<Resource<PostResponseModel>> = _postModelState.asStateFlow()
+
+    private val _postModelListState =
+        MutableStateFlow<Resource<List<PostResponseModel>>>(Resource.loading(null))
+    val postModelListState: StateFlow<Resource<List<PostResponseModel>>> =
+        _postModelListState.asStateFlow()
+
+    private val _clubModelListState =
+        MutableStateFlow<Resource<List<ClubPostResponseModel>>>(Resource.loading(null))
+    val clubModelListState: StateFlow<Resource<List<ClubPostResponseModel>>> =
+        _clubModelListState.asStateFlow()
+
+    private val _hotPlaceModelListState =
+        MutableStateFlow<Resource<List<HotPlaceResponsePostModel>>>(Resource.loading(null))
+    val hotPlaceModelListState: StateFlow<Resource<List<HotPlaceResponsePostModel>>> =
+        _hotPlaceModelListState.asStateFlow()
+
+    private val _isWished = MutableStateFlow(false)
+    val isWished: StateFlow<Boolean> = _isWished.asStateFlow()
+
+    suspend fun getWishMeeting() {
+        viewModelScope.launch {
+            _postModelState.value = Resource.loading(null)
+            try {
+                val response = repository.getWishMeetingPost(uId)
+                if (response.isSuccessful && response.body() != null) {
+                    val postList = response.body()
+                    _wishMeetingList.value = postList!!
+                    _postModelListState.value = Resource.success(postList)
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 에러 응답: $errorBody")
+                    _postModelListState.value =
+                        Resource.error(response.errorBody().toString(), null)
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+                _postModelListState.value = Resource.error(e.message ?: "An error occurred", null)
+            }
+        }
+    }
+
+    suspend fun getWishClub() {
+        viewModelScope.launch {
+            _postModelState.value = Resource.loading(null)
+            try {
+                val response = repository.getWishClubPost(uId)
+                if (response.isSuccessful && response.body() != null) {
+                    val postList = response.body()
+                    _wishClubList.value = postList!!
+                    _clubModelListState.value = Resource.success(postList)
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 에러 응답: $errorBody")
+                    _clubModelListState.value =
+                        Resource.error(response.errorBody().toString(), null)
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+                _clubModelListState.value = Resource.error(e.message ?: "An error occurred", null)
+            }
+        }
+    }
+
+    suspend fun getWishHotPlace() {
+        viewModelScope.launch {
+            _postModelState.value = Resource.loading(null)
+            try {
+                val response = repository.getWishHotPlacePost(uId)
+                if (response.isSuccessful && response.body() != null) {
+                    val postList = response.body()
+                    _wishHotPlaceList.value = postList!!
+                    _hotPlaceModelListState.value = Resource.success(postList)
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 에러 응답: $errorBody")
+                    _hotPlaceModelListState.value =
+                        Resource.error(response.errorBody().toString(), null)
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+                _hotPlaceModelListState.value =
+                    Resource.error(e.message ?: "An error occurred", null)
+            }
+        }
+    }
+
+//    suspend fun checkIsWishedPost(pId: Int, type: Int): Boolean {
+//        var result = mutableStateOf(false);
+//        viewModelScope.launch {
+//            try {
+//                val response = repository.checkIsWishedMeetingPost(uId, pId, type)
+//                if (response.isSuccessful && response.body() != null) {
+//                    val isExist = response.body()
+//                    if (isExist != null) {
+//                        _isWished.value = isExist
+//                        result.value = isExist
+//                    }
+//                } else {
+//                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+//                    Log.e("API Error", "포스트 에러 응답: $errorBody")
+//                }
+//            } catch (e: Exception) {
+//                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+//            }
+//        }
+//        Log.d("미란링 wishViewModel", result.value.toString())
+//        return result.value
+//    }
+
+    suspend fun checkIsWishedPost(pId: Int, type: Int): Boolean {
+        viewModelScope.launch {
+            try {
+                val response = repository.checkIsWishedMeetingPost(uId, pId, type)
+                if (response.isSuccessful && response.body() != null) {
+                    val isExist = response.body()
+                    if (isExist != null) {
+                        _isWished.value = isExist
+                    }
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 에러 응답: $errorBody")
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+            }
+        }
+        Log.d("미란링 wishViewModel", _isWished.value.toString())
+        return _isWished.value
+    }
+
+    fun addWishList(pId: Int, type: Int) {
+        val wish = when (type) {
+            1 -> {
+                WishListModel(
+                    pId,
+                    null,
+                    null,
+                    uId
+                )
+            }
+
+            2 -> {
+                WishListModel(
+                    null,
+                    pId,
+                    null,
+                    uId
+                )
+            }
+
+            else -> {
+                WishListModel(
+                    null,
+                    null,
+                    pId,
+                    uId
+                )
+            }
+        }
+
+        Log.d("미란 wishClub", wish.toString())
+        viewModelScope.launch {
+            _postModelState.value = Resource.loading(null)
+            try {
+                val response = when (type) {
+                    1 -> repository.addWishPost(wish)
+                    2 -> repository.addWishClub(wish)
+                    else -> repository.addWishPlace(wish)
+                }
+                if (response.isSuccessful && response.body() != null) {
+                    val wishList = response.body()
+                    when (type) {
+                        1 -> {
+                            _wishMeetingList.value += wishList as PostResponseModel
+                        }
+
+                        2 -> {
+                            _wishClubList.value += wishList as ClubPostResponseModel
+                        }
+
+                        else -> {
+                            Log.d("미란 이전 핫플", _wishHotPlaceList.value.toString())
+                            _wishHotPlaceList.value += wishList as HotPlaceResponsePostModel
+                            Log.d("미란 이후 핫플", _wishHotPlaceList.value.toString())
+                        }
+                    }
+                    _isWished.value = true
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 에러 응답: $errorBody")
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+            }
+        }
+    }
+
+    fun deleteWishList(pId: Int, type: Int) {
+        viewModelScope.launch {
+            _postModelState.value = Resource.loading(null)
+            try {
+                val response = repository.deleteWishList(uId, pId, type)
+                if (response.isSuccessful && response.body() != null) {
+                    val deleted = response.body()
+                    if (deleted != null) {
+                        _isDeleted.value = deleted
+                        _isWished.value = !deleted
+                    }
+                } else {
+                    val errorBody = response.errorBody()?.string() ?: "Unknown error"
+                    Log.e("API Error", "포스트 에러 응답: $errorBody")
+                }
+            } catch (e: Exception) {
+                Log.e("API Exception", "요청 중 예외 발생: ${e.message}")
+            }
+        }
+    }
+
+    fun changeWish(pId: Int, type: Int, wished: Boolean) {
+        if (wished) {
+            deleteWishList(pId, type)
+        } else {
+            addWishList(pId, type)
+        }
+    }
+}
